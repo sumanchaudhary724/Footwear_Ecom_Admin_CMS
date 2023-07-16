400db6f5c239215006eae38ac11df63460c70b4d
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-07-16 14:21:38 +1000

    Initialized project admin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Signin from "./components/pages/signin-signup/Signin";
+import Signup from "./components/pages/signin-signup/Signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+
+        {/*private router */}
+        <Route path="/new-admin" element={<Signup />} />
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
